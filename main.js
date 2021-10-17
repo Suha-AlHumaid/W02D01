fb146c30698d68c8f0e21b8bbde2cd58a201e68b
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-17 18:14:54 +0300

    login

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//function
+//1
 const sum = function (a, b) {
   // TODO: Your code here
   return a + b;
@@ -6,6 +8,7 @@ const sum = function (a, b) {
 sum(5, 10); // => 15
 sum(15, 10); // => 25
 
+//2
 const average = function (a, b) {
   // TODO: Your code here
   let result = (a + b) / 2;
@@ -15,6 +18,7 @@ const average = function (a, b) {
 average(20, 5); // => 12.5
 average(10, 20); // => 15
 
+//3
 /*  
    5! = 5 * 4 * 3 * 2 * 1
  */
@@ -37,6 +41,7 @@ const findFactorial = function (num) {
     findFactorial(11) // => 39916800
     */
 
+//4
 const round = function (number) {
   // TODO: Your code here
   let result = Math.round(number);
@@ -48,6 +53,7 @@ round(2.4); // => 2
 round(0.8); // => 1
 round(1.7); // => 12
 
+//5
 const toThePowerOf = function (base, exponent) {
   // TODO: Your code here
   let result = Math.pow(base, exponent);
@@ -58,6 +64,7 @@ toThePowerOf(2, 2); // => 4
 toThePowerOf(2, 3); // => 8
 toThePowerOf(9, 2); // => 81
 
+//6
 const randomNumber = function () {
   // TODO: Your code here
 
@@ -69,6 +76,7 @@ randomNumber(); // => 0.5052417714491793
 randomNumber(); // => 0.32678224417562673
 randomNumber(); // => 0.43265697184952523
 
+//7
 const oneOrZero = function () {
   // TODO: Your code here
   let result = Math.round(Math.random()); // random btw range 0 and 1 and round method will return the nearest integer.
@@ -79,6 +87,7 @@ oneOrZero(); // => 1
 oneOrZero(); // => 1
 oneOrZero(); // => 0
 
+//8
 const randomRange = function (number) {
   // TODO: Your code here
   return Math.random() * number;
@@ -90,6 +99,7 @@ randomRange(10); // => 7
 randomRange(10); // => 1
 randomRange(10); // => 0
 
+//9
 const includeOf = function (string, character) {
   return string.includes(character);
   // TODO: Your code here
@@ -134,18 +144,38 @@ updateOrCreate([1, 2, 3], 1, 10); // => [1, 10, 3]
 const onlyString = function (array) {
   // TODO: Your code here
   let newArray = [];
-for (let i=0 ; i< array.length; i++)
-{
- if (typeof(array[i]) === typeof(""))
- {
-   newArray.push(array[i]);
- }
-}
-return newArray;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === typeof "") {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
-onlyString(["one",12,"hi",true , 88 , "12"]); // => ["one","hi","12"]
-onlyString([10, 20, 30, 40, 50, 60,false]); // => []
+onlyString(["one", 12, "hi", true, 88, "12"]); // => ["one","hi","12"]
+onlyString([10, 20, 30, 40, 50, 60, false]); // => []
+
 //5
+const users = [
+  ["Jane", "123456"],
+  ["admin", "abc123"],
+  ["suha", "1234"],
+];
+
+const login = function (username, password) {
+  // TODO: Your code here
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i][0] == username && users[i][1] == password) {
+      return "Login Successful";
+    }
+  }
+  return "Login Failed";
+};
+
+login("Jane", "123456"); // => "Login Successful"
+login("Jane", "5321"); // => "Login Failed"
+login("suha", "1234"); // => "Login Successful"
+login("admin", "abc123");
 
 //6
 const maximumNumber = function (numbers) {
